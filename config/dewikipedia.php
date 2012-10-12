c1bd1e42055a5c0e6e7fb65b054668e50529b5c0
Marco Schuster <marco+swd@m-s-d.eu>
2012-10-13 00:16:31 +0200

    config: added siteinfo of german wikipedia (namespace names)

--- config/dewikipedia.php
+++ config/dewikipedia.php
@@ -2,4 +2,6 @@
 $name="dewiki";				//project name (free to choose)
 $wiki_url="http://de.wikipedia.org";	//base URL of wiki
 $api_url="$wiki_url/w/api.php";			//API endpoint
+$siteinfo=unserialize(file_get_contents("$confdir/dewikipedia.info"));
+$siteinfo=$siteinfo["query"];
 ?>
